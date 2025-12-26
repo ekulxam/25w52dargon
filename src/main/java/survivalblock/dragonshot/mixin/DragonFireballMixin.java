53ea4646cd4aa56428fb76b23b25ba3b6510b6f9
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-12-26 01:05:03 -0800

    Add TODO in DragonFireballMixin

--- src/main/java/survivalblock/dragonshot/mixin/DragonFireballMixin.java
+++ src/main/java/survivalblock/dragonshot/mixin/DragonFireballMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
 import net.minecraft.world.entity.projectile.hurtingprojectile.DragonFireball;
 import org.spongepowered.asm.mixin.Mixin;
 
+// TODO: dragon's breath gradually increases in height too?
 @Mixin(DragonFireball.class)
 public class DragonFireballMixin extends AbstractHurtingProjectileMixin {
     @Override
